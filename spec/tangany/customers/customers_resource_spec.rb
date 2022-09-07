4a1d00b7d611bd53a84bfa7bddcaf42036bc3f96
Matteo Panara <matteo.panara@gmail.com>
2022-09-07 14:09:55 +0000

    fix(bp-520): flaky customers delete test

--- spec/tangany/customers/customers_resource_spec.rb
+++ spec/tangany/customers/customers_resource_spec.rb
@@ -258,6 +258,6 @@ RSpec.describe(Tangany::Customers::CustomersResource) do
       next unless id.match?(/[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}/i)
 
       id
-    end.compact[0]
+    end.compact.min
   end
 end
