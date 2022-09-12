9e39030ab93d3e7779b36edbe98887f3f1f766f0
Matteo Panara <matteo.panara@gmail.com>
2022-09-12 13:38:52 +0000

    test(bp-520): test coverage 100%

---
+++ spec/tangany/request_error_spec.rb
@@ -0,0 +1,28 @@
+RSpec.describe Tangany::RequestError do
+  describe "#new" do
+    subject(:error) { described_class.new(message, validation_errors: validation_errors) }
+
+    let(:message) { "foo" }
+
+    context "without validation errors" do
+      let(:validation_errors) { nil }
+
+      it "builds the correct error message" do
+        expect(error.message).to eq(message)
+      end
+    end
+
+    context "with some validation errors" do
+      let(:validation_errors) {
+        [{
+          message: "bar",
+          source: "baz"
+        }]
+      }
+
+      it "builds the correct error message" do
+        expect(error.message).to eq("foo Validation errors: baz: bar")
+      end
+    end
+  end
+end
