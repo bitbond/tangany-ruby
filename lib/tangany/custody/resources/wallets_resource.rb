423dc7ce6b9bc165b0e92c595a70f7c7e4724a52
Matteo Panara <matteo.panara@gmail.com>
2022-09-16 13:52:45 +0000

    chore(bp-520): removed unnecessary code

--- lib/tangany/custody/resources/wallets_resource.rb
+++ lib/tangany/custody/resources/wallets_resource.rb
@@ -5,12 +5,6 @@ module Tangany
         safe_params = Wallets::ListContract.new.to_safe_params!(params)
         Collection.from_response(get_request("wallets", params: safe_params), type: Wallet)
       end
-
-      private
-
-      def serialize_tags(tags)
-        tags.present? ? tags.join(",") : nil
-      end
     end
   end
 end
