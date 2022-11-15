d4ec9d4495e8dd425445a2e8273108efe5b9aa1b
Matteo Panara <matteo.panara@gmail.com>
2022-11-15 14:34:58 +0000

    refactor(bp-568): rspec it style

--- spec/tangany/customers/client_spec.rb
+++ spec/tangany/customers/client_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe(Tangany::Customers::Client) do
   context "when initialized" do
     subject(:client) { described_class.new }
 
-    it {expect(client.connection).to(be_a(Faraday::Connection))}
-    it {expect(client.subscription).to(eq(Tangany.subscription))}
+    it { expect(client.connection).to(be_a(Faraday::Connection)) }
+    it { expect(client.subscription).to(eq(Tangany.subscription)) }
   end
 end
